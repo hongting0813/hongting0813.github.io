766ca2e5818d16c5a49cc32ff31b27e9c36022bb
SP12893678 <office1289366@gmail.com>
2021-04-05 18:27:31 +0800

    add eslint setting

--- App.vue
+++ App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app class="container">
+  <v-app class="my-container">
     <v-app-bar
       app
       shrink-on-scroll
@@ -8,37 +8,77 @@
       height="140"
       min-width="240"
     >
+      <v-app-bar-nav-icon
+        dark
+        @click.stop="drawer = !drawer"
+      >
+      </v-app-bar-nav-icon>
+
       <v-img
         class="ma-5 ml-3 mt-2"
         max-height="100"
         max-width="175"
         :src="logo"
-      ></v-img>
+      />
     </v-app-bar>
-      <v-main class="ma-8"> 
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      width="280px"
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="ma-8">
       <h1>
         億能手袋電壓製品廠
       </h1>
-      </v-main>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import Vue from "vue";
+import Vue from 'vue'
 import logo from './src/assets/img/logo-word.png'
 
 export default Vue.extend({
-  data() {
-    return {
-      logo: logo,
-      bundler: "Parcel"
-    };
-  }
-});
+    data () {
+        return {
+            logo: logo,
+            bundler: 'Parcel',
+            drawer: false
+        }
+    }
+})
 </script>
 
 <style scoped>
-.container {
+.my-container {
   max-width: none;
   background: linear-gradient(to bottom, #323232 0%, #3F3F3F 40%, #1C1C1C 150%), linear-gradient(to top, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.25) 200%);
   background-blend-mode: multiply;
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import Vue from 'vue';
+import Vue from 'vue'
 import vuetify from './src/plugins/vuetify'
-import App from './App.vue';
+import App from './App.vue'
 
-new Vue({ 
-    vuetify,
-    render: createElement => createElement(App) 
-}).$mount('#app');
+new Vue({
+  vuetify,
+  render: createElement => createElement(App)
+}).$mount('#app')
