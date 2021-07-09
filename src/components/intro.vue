aa02745c2ac8312ade206ad0dd4180fbc572f601
Emily Luo <hongtingluo0813@gmail.com>
2021-07-09 23:40:37 +0800

    update

--- src/components/intro.vue
+++ src/components/intro.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <div class="bigTitle whiteText centerText elementCenter wow bounceInUp">
-      億能幫你達成10,000,000個可能
+      億能幫你達成10,000,000個可能 :)
     </div>
     <div :class="$vuetify.breakpoint.xs ? 'helloxs' :'hellolg' ">
       <h2>test</h2>
