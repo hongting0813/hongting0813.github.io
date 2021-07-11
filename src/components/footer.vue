a0fb11240016c8be7accfc163b70b9a6abc2c1d2
Emily Luo <hongtingluo0813@gmail.com>
2021-07-11 17:16:49 +0800

    update

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,8 +1,6 @@
 <template>
   <v-footer
-    app
     color="primary lighten-2"
-    padless
   >
     <v-row
       justify="center"
@@ -10,15 +8,16 @@
     >
       <v-col
         class="primary lighten-1 py-4 text-center white--text"
-        cols="12"
       >
         <v-row
           justify="center"
           align="center"
           class="pa-2"
         >
+          <!-- 1 -->
           <v-col
-            cols="2"
+            cols="12"
+            md="3"
           >
             <v-row justify="center">
               <v-col>
@@ -34,10 +33,15 @@
               </v-col>
             </v-row>
           </v-col>
+          <!-- 2 -->
           <v-col
-            cols="4"
+            cols="12"
+            md="3"
+            :class="($vuetify.breakpoint.mdAndDown?'mt-2':'')"
           >
-            <v-row justify="center">
+            <v-row
+              justify="center"
+            >
               <v-col>
                 <v-row justify="center">
                   <v-icon>
@@ -53,25 +57,25 @@
           </v-col>
         </v-row>
       </v-col>
-      <v-col
-        class="primary lighten-2 py-4 text-center white--text"
-        cols="12"
-      >
-        <v-row
-          justify="center"
-          class="ma-2"
-        >
-          億能手袋電壓製品廠 Yineng Manufactory
-        </v-row>
-
-        <v-row
-          justify="center"
-          class="ma-2"
-        >
-          ©{{ new Date().getFullYear() }}&nbsp;—&nbsp;<strong>億能 Yineng</strong>&nbsp;版權所有
-        </v-row>
-      </v-col>
     </v-row>
+    <v-col
+      class="primary lighten-2 py-4 text-center white--text"
+      cols="12"
+    >
+      <v-row
+        justify="center"
+        class="ma-2"
+      >
+        億能手袋電壓製品廠 Yineng Manufactory
+      </v-row>
+
+      <v-row
+        justify="center"
+        class="ma-2"
+      >
+        ©{{ new Date().getFullYear() }}&nbsp;—&nbsp;<strong>億能 Yineng</strong>&nbsp;版權所有
+      </v-row>
+    </v-col>
   </v-footer>
 </template>
 
--- src/components/page/intro.vue
+++ src/components/page/intro.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- banner -->
     <v-carousel
       cycle
       class="ma-0 pa-0"
@@ -20,6 +21,7 @@
         />
       </v-carousel-item>
     </v-carousel>
+    <!-- page1 -->
     <v-sheet
       :height="height"
       color="rgb(0,0,0,0)"
@@ -37,26 +39,26 @@
           >
             <div>
               億能位於
-              <span style="color:#ffe593aa;font-size:6vmin">深圳</span>
+              <span class="focusWord">深圳</span>
             </div>
-            <div>已有<span style="color:#ffe593aa;font-size:6vmin">20年</span>歷史</div>
+            <div>已有<span class="focusWord">20年</span>歷史</div>
             <div>是一家手袋製造工廠</div>
           </div>
         </v-col>
         <v-col
           md="5"
-          class="wow animate__slideInUp"
-          data-wow-delay="0.2s"
+          class="wow"
+          :class="[$vuetify.breakpoint.lgAndUp ? 'animate__fadeInRight' : 'animate__fadeInUp']"
         >
           <v-img
-            class="picBorder animate__backInDown"
+            class="picBorder"
             :src="enviroPng"
             :max-height="height*0.6"
           />
         </v-col>
       </v-row>
     </v-sheet>
-
+    <!-- page2 -->
     <v-sheet
       :height="height"
       color="rgb(0,0,0,0)"
@@ -83,19 +85,19 @@
                 ⻑期擔任
               </div>
               <div>
-                <span style="color:#ffe593aa;font-size:6vmin">歐美</span>
+                <span class="focusWord">歐美</span>
                 及
-                <span style="color:#ffe593aa;font-size:6vmin">日本</span>
+                <span class="focusWord">日本</span>
               </div>
-              <div>各大品牌的<span style="color:#ffe593aa;font-size:6vmin">OEM</span></div>
+              <div>各大品牌的 <span class="focusWord">OEM</span></div>
             </div>
           </v-row>
         </v-col>
         <v-col
           md="5"
           order-md="1"
-          class="wow animate__slideInUp"
-          data-wow-delay="0.2s"
+          class="wow"
+          :class="[$vuetify.breakpoint.lgAndUp ? 'animate__fadeInLeft' : 'animate__fadeInUp']"
         >
           <center>
             <v-img
@@ -103,44 +105,150 @@
               :src="worldmap"
               :style="{backgroundImage:'url('+worldmap+')'}"
               :max-height="height*0.6"
+            >
+              <span class="picWord textLeft">
+                <div><span style="color:#ffe593aa; font-size:7vmin;">O</span>riginal</div>
+                <div><span style="color:#ffe593aa; font-size:7vmin;">E</span>quipment</div>
+                <div><span style="color:#ffe593aa; font-size:6vmin;">M</span>anufacturer</div>
+              </span>
+            </v-img>
+          </center>
+        </v-col>
+      </v-row>
+    </v-sheet>
+    <!-- page3 -->
+    <v-sheet
+      :height="height+($vuetify.breakpoint.mdAndUp?0:(height*0.35*2.5))"
+      color="rgb(0,0,0,0)"
+      class="pa-5 ma-5"
+    >
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <div
+          class="bigTitle whiteText wow animate__fadeInDownBig textCenter"
+        >
+          <div>
+            擁有 <span class="focusWord">優良環境</span> 和 <span class="focusWord">資深員工</span>
+          </div>
+        </div>
+      </v-row>
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col
+          v-for="(item,i) in environment"
+          :key="i"
+          :cols="$vuetify.breakpoint.mdAndUp?5:12"
+        >
+          <v-row>
+            <v-col>
+              <v-img
+                class="picBorder pic wow animate__fadeInUp "
+                :height="$vuetify.breakpoint.mdAndDown?height*0.4:height*0.35"
+                :style="{backgroundImage:'url('+item.src+')'}"
+              />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-sheet>
+    <!-- page4 -->
+    <v-sheet
+      :height="height"
+      color="rgb(0,0,0,0)"
+      class="pa-5 ma-5"
+    >
+      <v-row
+        class="fill-height"
+        align="center"
+        justify="center"
+      >
+        <v-col md="5">
+          <div
+            class="bigTitle whiteText wow animate__fadeInLeft"
+            :class="[$vuetify.breakpoint.lgAndUp? 'textLeft' : 'textCenter']"
+          >
+            <div>
+              製造<span class="focusWord">優質</span>的產品
+            </div>
+            <div
+              style="font-size:3vmin;"
+              class="mt-5"
+            >
+              <span class="highlight">自行車防水袋、潛水用防水袋、運動防水背包等。</span>
+            </div>
+          </div>
+        </v-col>
+        <v-col
+          cols="12"
+          md="5"
+          class="wow"
+          :class="[$vuetify.breakpoint.lgAndUp ? 'animate__fadeInRight' : 'animate__fadeInUp']"
+        >
+          <center>
+            <v-img
+              :src="productPng"
+              :max-width="$vuetify.breakpoint.mdAndDown?height*0.8:height*1"
             />
           </center>
         </v-col>
       </v-row>
     </v-sheet>
+    <!-- page5 -->
+    <v-sheet
+      :height="height"
+      color="rgb(0,0,0,0)"
+      class="pa-5 ma-5"
+    >
+      <v-row
+        class="fill-height"
+        align="center"
+        justify="center"
+      >
+        <div
+          class="bigText  textCenter focusWord wow animate__zoomIn"
+          style="font-size:10vmin;"
+        >
+          Yineng<br>期待與您合作
+        </div>
+      </v-row>
+    </v-sheet>
+    <footerpage />
   </div>
 </template>
 
 <script>
-import pic1 from '../../assets/img/intro/banner.jpg'
-import locationPng from '../../assets/img/intro/location.png'
-import enviroPng from '../../assets/img/intro/enviro.jpg'
-import worldmap from '../../assets/img/intro/worldmap.jpg'
-// import pic2 from '../../assets/img/intro/intro2.png'
+import pic1 from '../../assets/img/intro/banner.jpg'// banner
+import enviroPng from '../../assets/img/intro/enviro.jpg'// page1
+import worldmap from '../../assets/img/intro/worldmap.jpg'// page2
+import enviro1 from '../../assets/img/environment/1.jpg'// page3
+import enviro2 from '../../assets/img/environment/2.jpg'// page3
+import enviro3 from '../../assets/img/environment/3.jpg'// page3
+import enviro4 from '../../assets/img/environment/4.jpg'// page3
+import productPng from '../../assets/img/intro/product.png'// page4
+import footerpage from '../footer.vue'
+
 export default {
+    components: {
+        footerpage
+    },
     data () {
         return {
-            colors: [
-                'indigo',
-                'warning',
-                'pink darken-2',
-                'red lighten-1',
-                'deep-purple accent-4'
-            ],
-            slides: [
-                'First',
-                'Second',
-                'Third',
-                'Fourth',
-                'Fifth'
-            ],
             gallery: [
                 { src: pic1 }
-                // { src: pic2 }
             ],
-            locationPng: locationPng,
             enviroPng: enviroPng,
-            worldmap: worldmap
+            worldmap: worldmap,
+            environment: [
+                { src: enviro1 },
+                { src: enviro2 },
+                { src: enviro3 },
+                { src: enviro4 }
+            ],
+            productPng: productPng
         }
     },
     computed: {
@@ -174,5 +282,31 @@ transform: scale(1.4);
 }
 .textLeft{
   text-align: left;
+}
+.picWord{
+  font-size:4vmin;
+  font-weight: 900;
+  color:white;
+  position: absolute;
+  text-shadow: black 0.1em 0.1em 0.2em;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+}
+.focusWord{
+  color:#ffe593aa;
+  text-shadow: black 0.1em 0.1em 0.2em;
+  font-size: 6vmin;
+  font-weight: 500;
+}
+.highlight-half{
+  background: linear-gradient(to top, #c0c0c0aa 35%, transparent 35%);
+}
+.highlight{
+  padding: 0 2px;
+  white-space: pre-line;
+  border-radius: 2px;
+  background-color: #c0c0c0aa;
 }
 </style>
