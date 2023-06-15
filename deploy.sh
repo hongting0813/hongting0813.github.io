5c98278c6a6d91f5747df3b44df5350a2fb5d10e
acs106104 <45888686+acs106104@users.noreply.github.com>
2023-06-15 18:32:19 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,6 @@ git commit -m 'deploy'
 git push -f git@github.com:hongting0813/hongting0813.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/hongting0813/Yineng.git main:gh-pages
+# git push -f https://github.com/hongting0813/Yineng.git main:gh-pages
 
 cd -
